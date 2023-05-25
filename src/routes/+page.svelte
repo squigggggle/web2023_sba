27a74261f3c674fea6c482cc8e47c04b4e005394
Jack Young (1000112537) <YOUNJO1@student.op.ac.nz>
2023-05-26 11:49:46 +1200

    feat: fetch with 6 random characters logged

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -80,6 +80,31 @@
 
 </style>
 
+<script>
+    //base link
+    const BASE_URL = "https://rickandmortyapi.com/api";
+    let characters = [];
+    // random page generator
+    let pageMin = 2;
+    let pageMax = 43;
+    let randomPage = Math.floor(Math.random() * (pageMax - pageMin) + pageMin);
+
+    let charMin = 0;
+    let charMax = 826;
+    //random characters
+    for(let i = 0; i < 6; i++){
+        characters.push(Math.floor(Math.random() * (charMax - charMin) + charMin))
+        
+    }
+    console.log(characters);
+    //fetch api
+    fetch(`${BASE_URL}/character/${characters}`)
+    .then(response => response.json())
+    .then(data => console.log(data));
+
+    //I can log the results of 6 random characters but I have no idea how to get them out of the fetch
+</script>
+
 <div class="grid">
 
     <div class="card">
